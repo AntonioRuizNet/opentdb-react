237b51fc3aea5bd0f8a0055842f3b36069554add
Antonio Ruiz <contacto.antonioruiz@gmail.com>
2021-09-10 12:40:09 +0200

    Create pages, result component & ApiCall

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+//Pages
+import Browse from './pages/Browse';
+import NotFound from './pages/NotFound';
+
+//Css
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App"></div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Browse} />
+        <Route exact path="/browse" component={Browse} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
